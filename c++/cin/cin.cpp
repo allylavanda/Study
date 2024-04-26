b46cb81a04ab513c9afe97dc093c47c246698f2a
allylavanda <allylavanda@aol.com>
2024-04-26 01:20:21 -0400

    cin

---
+++ c++/cin/cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int tip = 0;
+
+    std::cout << "Enter tip amount: ";
+    std::cin >> tip; // get input
+
+    // display output
+    std::cout << "You paid " << tip << " dollars.\n";
+}
